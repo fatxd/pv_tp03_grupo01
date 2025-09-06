9dccf463b58741d14f376e144e1a887fa4486e56
Verón Néstor Ezequiel <ezequielveron398@gmail.com>
2025-09-06 20:37:00 -0300

    Agregando CSS al ejercicio 1 EzequielVeron

--- ejercicio01/src/ejercicio01.js
+++ ejercicio01/src/ejercicio01.js
@@ -1,38 +1,42 @@
-let matriz = []
-
-for (let i = 0; i < 3; i++) {
-    let cadena = prompt("ingrese los valores");
-
-    matriz[i] = [];
-    for (let j = 0; j < cadena.length; j++) {
-        matriz[i][j] = cadena[j];
-
-    }
-}
-
-for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < matriz[i].length; j++) {
-
-        if (matriz[i][j] == "x") {
-            console.log("encontre la x en la fila: " + i + " columna:" + j);
-            let resta = 0;
-            let suma = 0;
-            if (i == 0) {
-
-                resta = Number(matriz[2][j] || 0) - Number(matriz[1][j] || 0);
-                 matriz[i][j] = resta;
-
-            } else if (i == 1) {
-                resta = Number(matriz[2][j] || 0) - Number(matriz[0][j] || 0);
-                matriz[i][j] = resta;
-
-            }else if (i == 2){
-                suma = Number(matriz[0][j] || 0) + Number(matriz[1][j] || 0);
-                matriz[i][j] = suma;
-            }
-
+let matriz = [];
+function obtenerNumeros() {
+    let valorX = null;
+    for (let i = 0; i < 3; i++) {
+        let cadena = document.getElementById("fila" + i).value;
+        matriz[i] = [];
+        for (let j = 0; j < cadena.length; j++) {
+            matriz[i][j] = cadena[j];
         }
+    }
 
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < matriz[i].length; j++) {
+            if (matriz[i][j] == "x") {
+                console.log("encontre la x en la fila: " + i + " columna:" + j);
+                if (i == 0) {
+                    valorX = Number(matriz[2][j] || 0) - Number(matriz[1][j] || 0);
+                } else if (i == 1) {
+                    valorX = Number(matriz[2][j] || 0) - Number(matriz[0][j] || 0);
+                } else if (i == 2) {
+                    valorX = Number(matriz[0][j] || 0) + Number(matriz[1][j] || 0);
+                    if (valorX > 9 && j > 0 ) {
+                        valorX = Math.floor(valorX % 10);
+                        if((Number(matriz[0][j+1]) + Number(matriz[1][j+1]) > 9)){
+                            valorX += 1; 
+                        }
+                    }else if(valorX >9 && (matriz[0][j+1] + matriz[1][j+1]) >9 ){
+                        valorX +=1;
+                    }
+                }
+                matriz[i][j] = valorX;
+            }
+        }
+    }
+    console.log(matriz);
+    if (valorX !== null) {
+        document.getElementById("salida").textContent = "El valor de X es: " + valorX;
+    } else {
+        document.getElementById("salida").textContent = "No se encontró ninguna X.";
     }
 }
-console.log(matriz);
+document.getElementById("btn-resultado").addEventListener("click", obtenerNumeros);
