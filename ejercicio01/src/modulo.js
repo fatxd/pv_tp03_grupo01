e1f7400ab2e1d4c576b961a7c322cc017ea11f47
Juan <jsebach99@gmail.com>
2025-09-08 19:38:54 -0300

    Actulizando archivo BachJuanSebastian

---
+++ ejercicio01/src/modulo.js
@@ -0,0 +1,10 @@
+export function cont_cadena(){
+    
+    for (let index = 0; index < 3; index++) {
+        let cadena = prompt("Ingrese numero:")
+       
+        
+    }
+
+    return cadena
+}
