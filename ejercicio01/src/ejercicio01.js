9757588b2a46a4bed99671a6c472e820fe6fbacc
Juan <jsebach99@gmail.com>
2025-09-10 16:49:23 -0300

    Agregando modularizacion Ejercicio01 Bach

--- ejercicio01/src/ejercicio01.js
+++ ejercicio01/src/ejercicio01.js
@@ -1,48 +1,15 @@
-<<<<<<< HEAD
-import { cont_cadena } from "./modulo.js";
+import { cargarMatriz, encontrarX } from "./modulo.js";
 
- 
-console.log(cont_cadena())
-=======
-let matriz = [];
-function obtenerNumeros() {
-    let valorX = null;
-    for (let i = 0; i < 3; i++) {
-        let cadena = document.getElementById("fila" + i).value;
-        matriz[i] = [];
-        for (let j = 0; j < cadena.length; j++) {
-            matriz[i][j] = cadena[j];
-        }
-    }
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < matriz[i].length; j++) {
-            if (matriz[i][j] == "x") {
-                console.log("encontre la x en la fila: " + i + " columna:" + j);
-                if (i == 0) {
-                    valorX = Number(matriz[2][j] || 0) - Number(matriz[1][j] || 0);
-                } else if (i == 1) {
-                    valorX = Number(matriz[2][j] || 0) - Number(matriz[0][j] || 0);
-                } else if (i == 2) {
-                    valorX = Number(matriz[0][j] || 0) + Number(matriz[1][j] || 0);
-                    if (valorX > 9 && j > 0 ) {
-                        valorX = Math.floor(valorX % 10);
-                        if((Number(matriz[0][j+1]) + Number(matriz[1][j+1]) > 9)){
-                            valorX += 1; 
-                        }
-                    }else if((matriz[0][j+1] + matriz[1][j+1]) >9 ){
-                        valorX +=1;
-                    }
-                }
-                matriz[i][j] = valorX;
-            }
-        }
-    }
-    console.log(matriz);
+function main() {
+   
+    let matriz = cargarMatriz()
+    let valorX = encontrarX(matriz)
+    console.log(matriz)
+    console.log(valorX)
     if (valorX !== null) {
         document.getElementById("salida").textContent = "El valor de X es: " + valorX;
     } else {
         document.getElementById("salida").textContent = "No se encontró ninguna X.";
     }
 }
-document.getElementById("btn-resultado").addEventListener("click", obtenerNumeros);
->>>>>>> origin/EzequielVeron
+document.getElementById("btn-resultado").addEventListener("click", main);
