a630e372f8f426fd15fa23d20458268a68808b56
franco arancibia <rlz.fateegg@gmail.com>
2025-09-10 21:37:57 -0300

    subiendo JS y arreglando errores Arancibia

--- ejercicio02/src/ejercicio02.js
+++ ejercicio02/src/ejercicio02.js
@@ -1,16 +1,42 @@
 
 let listaMascotas = [];
-let nombre = document.getElementById("fila0");
-let tipo = document.getElementById("fila1");
-let edad = document.getElementById("fila2");
-let nombreDueno = document.getElementById("fila3");
-let vacunaSelect = document.querySelector('input[name="vacu"]:checked');
-let vacuna = vacunaSelect ? vacunaSelect.value : "No seleccionado";
 
-function agregarMascota(nombre, tipo, edad, nombreDueno, vacuna) {
+function agregarMascota() {
+
+    let nombre = document.getElementById("nombre-mascota").value;
+    let tipo = document.getElementById("opciones").value;
+    let edad = document.getElementById("edad").value;
+    let nombreDueno = document.getElementById("nombre-dueno").value;
+    let vacunaSelect = document.querySelector('input[name="vacunado"]:checked');
+    let vacuna = vacunaSelect ? vacunaSelect.value : "No seleccionado";
     const mascota = { nombre, tipo, edad, nombreDueno, vacuna };
+
     listaMascotas.push(mascota);
+
     console.log(listaMascotas);
+    mostrarMascotas();
+
+    document.getElementById("formulario").reset();
+}
+function mostrarMascotas() {
+    let tabla = document.querySelector("#tabla-mascotas tbody");
+    tabla.innerHTML = ""; // limpiar antes de volver a pintar
+
+    listaMascotas.forEach(mascota => {
+        let fila = `
+          <tr>
+            <td>${mascota.nombre}</td>
+            <td>${"Raza: "+mascota.tipo}</td>
+            <td>${"Edad: "+mascota.edad}</td>
+            <td>${"Nombre del dueño: "+mascota.nombreDueno}</td>
+            <td>${"Vacunado: " +mascota.vacuna}</td>
+          </tr>
+        `;
+        tabla.innerHTML += fila;
+    });
 }
 
-document.getElementById("formulario").addEventListener("submit", agregarMascota)
+document.getElementById("formulario").addEventListener("submit", function (e) {
+    e.preventDefault(); // evita que la página se recargue
+    agregarMascota();
+});
