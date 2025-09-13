cb71ae6254d4906885749be1b4b4217382c41be9
Juan <jsebach99@gmail.com>
2025-09-13 19:06:28 -0300

    Subiendo Ejercicio 2 Bach

--- ejercicio02/src/ejercicio02.js
+++ ejercicio02/src/ejercicio02.js
@@ -3,41 +3,75 @@ let listaMascotas = [];
 
 function agregarMascota() {
 
-    let nombre = document.getElementById("nombre-mascota").value;
-    let tipo = document.getElementById("opciones").value;
-    let edad = document.getElementById("edad").value;
-    let nombreDueno = document.getElementById("nombre-dueno").value;
-    let vacunaSelect = document.querySelector('input[name="vacunado"]:checked');
-    let vacuna = vacunaSelect ? vacunaSelect.value : "No seleccionado";
-    const mascota = { nombre, tipo, edad, nombreDueno, vacuna };
+  let nombre = document.getElementById("nombre-mascota").value;
+  let tipo = document.getElementById("opciones").value;
+  let edad = document.getElementById("edad").value;
+  let nombreDueno = document.getElementById("nombre-dueno").value;
+  let vacunaSelect = document.querySelector('input[name="vacunado"]:checked');
+  let vacuna = vacunaSelect ? vacunaSelect.value : "No seleccionado";
+  const mascota = { id: Date.now(), nombre, tipo, edad, nombreDueno, vacuna };
 
-    listaMascotas.push(mascota);
+  listaMascotas.push(mascota);
 
-    console.log(listaMascotas);
-    mostrarMascotas();
 
-    document.getElementById("formulario").reset();
+  mostrarMascotas();
+
+  document.getElementById("formulario").reset();
 }
 function mostrarMascotas() {
-    let tabla = document.querySelector("#tabla-mascotas tbody");
-    tabla.innerHTML = ""; // limpiar antes de volver a pintar
+  let tabla = document.querySelector("#tabla-mascotas tbody");
+  tabla.innerHTML = ""; // limpiar antes de volver a pintar
 
-    listaMascotas.forEach(mascota => {
-        let fila = `
-          <tr>
-            <td>${mascota.nombre}</td>
-            <td>${"Raza: "+mascota.tipo}</td>
-            <td>${"Edad: "+mascota.edad}</td>
-            <td>${"Nombre del dueño: "+mascota.nombreDueno}</td>
-            <td>${"Vacunado: " +mascota.vacuna}</td>
-          </tr>
-        `;
-        tabla.innerHTML += fila;
-    });
+  listaMascotas.forEach(mascota => {
+    let fila = `
+  <tr data-id="${mascota.id}">
+    <td>${mascota.nombre}</td>
+    <td>${"Raza: " + mascota.tipo}</td>
+    <td>${"Edad: " + mascota.edad}</td>
+    <td>${"Nombre del dueño: " + mascota.nombreDueno}</td>
+    <td>${"Vacunado: " + mascota.vacuna}</td>
+    <td><button data-action="borrar">Eliminar</button></td>
+  </tr>
+`;
+    tabla.innerHTML += fila;
+  });
 }
 
 document.getElementById("formulario").addEventListener("submit", function (e) {
-    e.preventDefault(); // evita que la página se recargue
-    agregarMascota();
+  e.preventDefault(); // evita que la página se recargue
+  agregarMascota();
+
+
 });
+
+document.querySelector("#tabla-mascotas tbody").addEventListener("click", function (e) {
+  if (e.target.dataset.action === "borrar") {
+    let fila = e.target.closest("tr"); // busca la fila
+    let id = parseInt(fila.dataset.id); // obtiene el id
+    borrarDatos(e)
+    borrarMascota(id);
+    
+     mostrarMascotas(); // refresca la tabla
+  }
+});
+
+
+
+
+function borrarDatos(e) {
+    if (e.target.dataset.action === "borrar") {
+
+
+
+        let button = e.target
+        let mascotaId = button.closest('tr')
+        let id = parseInt(mascotaId.dataset.id)
+        borrarMascota(id);
+    }
+
+}
+function borrarMascota(id) {
+    listaMascotas = listaMascotas.filter(mascota => mascota.id !== id);
+
+}
 
